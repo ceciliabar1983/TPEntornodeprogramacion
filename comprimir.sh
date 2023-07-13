66fbf887d9220543149d1d5a1d90f4d3ecfd0043
Cecilia Baronio <baronio_cecilia@hotmail.com>
2023-07-12 22:29:02 -0300

    actualizacion vs

---
+++ comprimir.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+cd resultado/fotos
+sudo chmod +x *.jpg
+echo "Procesando las imágenes..."
+sleep 5s
+
+# Recortar las imágenes al tamaño deseado
+for foto in *; do
+   if [[ "$foto" =~ ^[A-Z][a-z]+$ ]]; then
+      sudo convert "$foto" -gravity center -resize 512x512+0+0 -extend 512x512 "$foto"
+   fi
+done
+
+echo "Las imágenes fueron procesadas exitosamente."
+cd ..
+./menu.sh
+exit 0
+else
+	echo " No se encuentra el directorio"
+cd ..
+./menu.sh
+exit 1
+fi
---
+++ descomprimir.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Verificamos si existe el archivo descargado o se creó en los scripts anteriores; si existe, se descomprime
+if [[ -e ./resultado/"imagenes.zip" ]]; then
+        sudo unzip ./resultado/"imagenes.zip" -d resultado
+        echo "Se descomprimió correctamente."
+        sudo rm -r resultado/imagenes.zip
+	./menu.sh
+        exit 0
+
+else
+         echo "El archivo imagenes.zip no existe."
+	./menu.sh
+	 exit 1
+fi
+
