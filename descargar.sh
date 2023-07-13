5b28c7be0f2210a7e530c934adad4b9a135ff98d
ceciliabar1983 <94814162+ceciliabar1983@users.noreply.github.com>
2023-07-13 14:15:49 -0300

    Update descargar.sh

--- descargar.sh
+++ descargar.sh
@@ -1,30 +1,26 @@
 #!/bin/bash
-url_imagenes=$1
-url_sumaverificacion=$2
+
 
 # Verifica si se han proporcionado las dos URLs solicitadas como argumentos
 if [ $# -eq 2 ]; then
 #Descargar la imagen comprimida y la suma de verificacion
 
-	wget $url_imagenes  -O resultado/"imagenes.zip"
-	wget -O resultado/sumaverificacion.txt  $url_sumaverificacion
- 
-   	# Verificamos las  suma de verificacion coicidan
-	suma_verificacion_imagenes_generadas=$(sha256sum resultado/imagenes.zip | cut -d " " -f 1)
-
-	suma_verificacion_imagenes_descargadas=$(cat resultado/sumaverificacion.txt) 
+	sudo wget $1  -O resultado/"fotos.zip"
+ 	suma_verificacion_imagenes_generadas=$(sha256sum resultado/fotos.zip | tr -d " " )
+	sudo  $2 wget -O resultado/sumaverificacion.txt  
+ 	suma_verificacion_imagenes_descargadas=$(cat resultado/sumaverificacion.txt) 
 
 #Verificamos que coincidan 
 
-	if [[ $suma_verificacion_imagenes_generadas =~  $suma_verificacion_imagenes_descargadas ]]; then
-	echo "las sumas de verificacion no coinciden"
-	./menu.sh
-	exit 0
+	if [[ $suma_verificacion_imagenes_generadas ==  $suma_verificacion_imagenes_descargadas ]]; then
+		echo "las sumas de verificacion coinciden"
+		./menu.sh
+		exit 0
 	else
-	echo "las sumas de verificacion coinciden"
-	./menu.sh
-	exit 1
-	fi
+		echo "las sumas de verificacion coinciden"
+		./menu.sh
+		exit 1
+		fi
 else
 	echo " verifique los parametros ingresados"
 	./menu.sh
