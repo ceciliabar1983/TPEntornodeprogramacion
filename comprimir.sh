6ff3433c4367dce4311e7a95282d22127171f208
ceciliabar1983 <94814162+ceciliabar1983@users.noreply.github.com>
2023-07-13 14:20:00 -0300

    Update comprimir.sh

--- comprimir.sh
+++ comprimir.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
-cd resultado/fotos
-sudo chmod +x *.jpg
-echo "Procesando las imágenes..."
-sleep 5s
-
-# Recortar las imágenes al tamaño deseado
-for foto in *; do
-   if [[ "$foto" =~ ^[A-Z][a-z]+$ ]]; then
-      sudo convert "$foto" -gravity center -resize 512x512+0+0 -extend 512x512 "$foto"
-   fi
-done
-
-echo "Las imágenes fueron procesadas exitosamente."
+cd resultado
+if [ -d fotos ]; then
+# Generar archivo con la lista de nombres de todas las imágenes
+	ls fotos > nombres_imagenes.txt
+#Generar una lista con los nombres válidos
+	ls fotos | sudo grep -oE '^[A-Z][a-z]+$' > lista_nombres_validos.txt
+# Generar archivo con el total de personas cuyo nombre finaliza con la letra 'a'
+	ls fotos |sudo grep -c '^[A-Z][a-z][a]$' > total_personas_comienzan_con_a.txt
+# Crear archivo comprimido que incluya todos los archivos generados y las imágenes procesadas
+	sudo zip -r imagenesdefinitivas.zip fotos nombres_imagenes.txt lista_nombres_validos.txt total_personas_comienzan_con_a.txt
+	sudo rm -r fotos nombres_imagenes.txt lista_nombres_validos.txt total_personas_comienzan_con_a.txt
+echo "Se comprimieron las imagenes"
 cd ..
 ./menu.sh
 exit 0
 else
-	echo " No se encuentra el directorio"
-cd ..
-./menu.sh
+	echo " no se pudieron comprimir las imagenes"
+	./menu.sh
 exit 1
 fi
